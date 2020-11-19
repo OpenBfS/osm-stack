aa5ea76f167911d8abc695c79ccc42f11925142d
bs-fr <sburbeck@bfs.de>
2020-11-19 08:16:47 +0100

    revert network name change in pg_hba.conf

--- bfs-postgres/init_nominatim.sh
+++ bfs-postgres/init_nominatim.sh
@@ -8,6 +8,6 @@ createdb nominatim
 psql -d nominatim -c "CREATE EXTENSION postgis; CREATE EXTENSION hstore;"
 
 cat <(head -n -2 /var/lib/postgresql/data/pg_hba.conf) <(echo "
-host	all	all	$PGNET_OSM	trust
+host	all	all	$PGNET	trust
 ") > /tmp/pg_hba.conf
 mv /tmp/pg_hba.conf /var/lib/postgresql/data/pg_hba.conf
