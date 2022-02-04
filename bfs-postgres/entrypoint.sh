75b758ef8601763ac200cd24ad12e74cb6877c25
Michael Reichert <michael.reichert@geofabrik.de>
2022-02-04 12:42:28 +0100

    Bugfix: Postgres image for Nominatim
    The special image for Postgres with Nominatim does not have special
    CheckMK checks. Therefore, writing their configuration will fail.

--- bfs-postgres/entrypoint.sh
+++ bfs-postgres/entrypoint.sh
@@ -2,10 +2,12 @@
 
 set -e
 
-/bin/bash /write_pg_replication_checker_conf.sh
+if [ -f "/usr/lib/check_mk_agent/local/mk_postgres_replication.py" ]; then
+    /bin/bash /write_pg_replication_checker_conf.sh
 
-if [ -f /var/lib/postgresql/data/postgresql.conf ] ; then
-    /update_postgresql_conf.sh /var/lib/postgresql/data/postgresql.conf
+    if [ -f /var/lib/postgresql/data/postgresql.conf ] ; then
+        /update_postgresql_conf.sh /var/lib/postgresql/data/postgresql.conf
+    fi
 fi
 
 xinetd
