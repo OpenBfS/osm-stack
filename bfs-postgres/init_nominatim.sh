4a2b94b1d3fb1899987011d4f4d49843b4aed8e7
Michael Reichert <michael.reichert@geofabrik.de>
2022-02-10 15:20:24 +0100

    Run createdb and createuser as user postgres

--- bfs-postgres/init_nominatim.sh
+++ bfs-postgres/init_nominatim.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-createuser --superuser nominatim
-createdb nominatim
+su postgres -c "createuser --superuser nominatim"
+su postgres -c "createdb nominatim"
 
-psql -d nominatim -c "CREATE EXTENSION postgis; CREATE EXTENSION hstore;"
+psql -U postgres -d nominatim -c "CREATE EXTENSION postgis; CREATE EXTENSION hstore;"
 
 cat <(head -n -2 /var/lib/postgresql/data/pg_hba.conf) <(echo "
 host	all	all	$PGNET	trust
