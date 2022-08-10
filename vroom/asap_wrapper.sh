734f526a7b502cc1143f019ba1b20916494ee391
bs-fr <sburbeck@bfs.de>
2022-08-10 12:00:13 +0200

    use vroom ASAP --pareto-front-more-solutions

--- vroom/asap_wrapper.sh
+++ vroom/asap_wrapper.sh
@@ -2,4 +2,5 @@
 
 set -euo pipefail
 
-python3 /vroom-scripts/src/asap.py $@
+python3 /vroom-scripts/src/asap.py --pareto-front-more-solutions $@
+#python3 /vroom-scripts/src/asap.py --pareto-front $@
